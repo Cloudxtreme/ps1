fb5064abf9f4ab79413561d4a5d0889df0d8631b
Charles Merriam <charles.merriam@gmail.com>
2017-03-16 18:20:25 -0700

    Expand sanity test to make it run with BDD basics

--- src/test_sanity.js
+++ src/test_sanity.js
@@ -1,7 +1,27 @@
 /* Really basic JavaScript tests, to see if mocha is running */
+require('should');  // side effect is to modify object with 'should' getter.
 
-describe('Test', () => {
-  describe('simple math works', () => {
-    it('should count to 4', () => 1 + 1 + 1 + 1);
+function four() {
+  return 1 + 1 + 1 + 1;
+}
+
+describe('Basic Sanity Test', () => {
+  describe('different ways to test', () => {
+    it('should count to 4 without crashing', () => 1 + 1 + 1 + 1);
+    it('should assert being calling a function', function count() {
+      const assert = require('assert');
+      assert(four() === 4);
+    });
+    it('should do the should examples', function done() {
+      (5).should.be.exactly(5).and.be.a.Number();
+    });
+    it('should be four using BDD syntax', function done() {
+      (four()).should.equal(4);
+    });
+    it('but to five shall thou not count', function count() {
+      four().should.be.a.Number();
+      (four()).should.not.equal(5);
+    });
+    it('should arrow function to four', () => four().should.be.exactly(4));
   });
 });
