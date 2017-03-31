6208273328637debc2b831513cf96472b601d089
Charles Merriam <charles.merriam@gmail.com>
2017-03-31 15:14:44 -0700

    Add an 'exterminate' command to kill everything on DigitalOcean
    Note that there may well be a minor bug; digitalOcean appears to return
    immediately, not when the drops are really deleted, so exterminate
    might still list drops as not deleted when they really all.   Looking
    into it.

---
+++ deploy/exterminate.js
@@ -0,0 +1,23 @@
+import console from 'better-console';
+import Ocean from './ocean';
+
+
+function line(theChar) {
+  return `${Array(60).join(theChar)}\n`;
+}
+
+const ocean = new Ocean();
+console.log(Array(5).join(line('=')));
+
+ocean.getListOfDrops()
+  .then((drops) => {
+    console.log('Dropping Old state:');
+    console.log(Ocean.prettyDrops(drops));
+    return ocean.destroyDrops();
+  })
+  .then(() => ocean.getListOfDrops())
+  .then((drops) => {
+    console.log('New state:');
+    console.log(Ocean.prettyDrops(drops));
+  })
+  .catch(err => console.log('error: ', err));
