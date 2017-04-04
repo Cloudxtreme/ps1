6d788e1f4f82c8d87d65258e2e79db9c179c4d7b
Charles Merriam <charles.merriam@gmail.com>
2017-04-04 10:07:53 -0700

    Tracing adds a few new tricks...
    Needs refactoring and clean into a good test

--- tracer.js
+++ tracer.js
@@ -2,7 +2,13 @@ const o = console.log;
 
 o('.1.');
 o('.2.');
-setTimeout((() => { o('First timeout finished'); }), 1000);
+setTimeout(   // a function with two parameters
+    () => {   // first parameter is an arrow function
+      o('First timeout finished');
+    }  // end of the arrow function
+  , 1000,    // second parameter, 1000 seconds
+);   // end of set timeout.
+setTimeout(() => { o('First timeout finished'); }, 1000);
 o('.3.');
 
 function wait(milliseconds, value = undefined) {
@@ -28,15 +34,15 @@ console.log('.5.');
 const promiseC = promiseA.then((value) => {
   o(`in promiseA.then with value ${value}`);
   o('which means promiseA must have resolved');
-  throw 'aa';
   return wait(1000, 'promiseC');
 });
 console.log('.6.');
-// const d = promiseC.then((value) => {
-//  o(`in promiseC.then with value ${value}`);
-// });
-const e = promiseC.catch((errorMessage) => {
+const promiseD = promiseC.then((value) => {
+  o(`in promiseC.then with value ${value}`);
+});
+const promiseE = promiseC.catch((errorMessage) => {
   o(`in promiseC.catch with err ${errorMessage}`);
 });
-console.log(`Notice and ${e}`);
+console.log(`  promiseD is a ${promiseD}, because then() is always a promise.`);
+console.log(`  promiseE is a ${promiseE}, because catch() is always a promise.`);
 console.log('.7.');
