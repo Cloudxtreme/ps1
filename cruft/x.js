26d644f469106ea9654e6533c64a6d1a97efb07d
Charles Merriam <charles.merriam@gmail.com>
2017-04-10 21:59:42 -0700

    Add to my testbed on promise logging.

--- cruft/x.js
+++ cruft/x.js
@@ -1,51 +1,74 @@
 import console from 'better-console';
-import { nodeReportUnhandledPromises, wait, poll } from '../src/junkDrawer';
-import d from '../src/logging';
+import { nodeReportUnhandledPromises, wait, line } from '../src/junkDrawer';
+// import d from '../src/logging';
 
-function dp(p = undefined) {
+function dp(...params) {
   // return a new debuggable promise,
   // call like:  `dp().then(thing)...`;
-  return new DebugPromise(p);
+  return new DebugPromise(...params);
 }
 
-let nextId = 100;
+let nextId = 1;
+let nextGroup = 0;
+const groupIds = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
 const plist = {};
 
-function hprint(id) {
-  console.log('%d | %s | ', id, plist[id].history, plist[id].pobj);
+
+function hp(msg) {
+  console.log('prolog: ', msg);
+  // set to history?
 }
 
 class DebugPromise {
-  constructor(p = undefined) {
+  constructor(p, group, text) {
     this.p = p || Promise.resolve('no argument');
-    this.id = nextId;
-    plist[nextId] = { history: `created ${nextId}`, pobj: this.p };
+    if (group) {
+      this.group = group || '';
+    } else {
+      this.group = groupIds[nextGroup];
+      nextGroup += 1;
+    }
+    this.id = this.group + nextId.toString();
+    if (group) {
+      hp(`${text}${this.id}`);
+    } else if (p) {
+      hp(`new ${this.id} chain from argument`);
+    } else {
+      hp(`new ${this.id} chain`);
+    }
+    plist[this.id] = { pobj: this.p, group: this.group };
     nextId += 1;
-    hprint(this.id);
+    // hprint(this.id);
   }
 
   then(...params) {
-    d(`in then for ${this.id}`);
-    const p2 = dp(this.p.then(...params));
-    plist[this.id].history = `resolved in then, making ${p2.id}`;
-    console.dir(plist[this.id]);
-    hprint(this.id);
+    const p1 = this.p.then(...params);
+    const p2 = dp(p1, this.group, `(${this.id}).then new `);
+    plist[this.id].history = `then -> ${p2.id}`;
+    p1.then(
+      () => { hp(`${p2.id} fulfills`); },
+      () => { hp(`${p2.id} rejects`); });
     return p2;
   }
 
   catch(...params) {
-    const p2 = dp(this.p.catch(...params));
-    plist[this.id].history = `resolved in catch, making ${p2.id}`;
-    hprint(this.id);
-    return dp(this.p.catch(...params));
+    const p1 = this.p.catch(...params);
+    const p2 = dp(p1, this.group, `(${this.id}).catch new `);
+    plist[this.id].history = `catch ->  ${p2.id}`;
+    p1.then(
+      () => { hp(`${p2.id} fulfills`); },
+      () => { hp(`${p2.id} rejects`); });
+    return p2;
   }
 }
 
 
 function f5() {
-  dp().then(() => wait(1000)).then(() => wait(500));
+  dp().then(() => wait(1000)).then(() => wait(500)).then(() => 'done');
   console.log('2......');
   dp(wait(1200)).then(() => wait(500));
+  // wait(5000).then(hprint);
+  //  hprint();
 }
 
 function f1() {
@@ -68,4 +91,5 @@ function f3() {
   // console.log(e.lineNumber);
 }
 
+console.log(Array(5).join(line('=')));
 f5();
