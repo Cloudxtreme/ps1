b1182bde586d42835bfb5782d643700564027cbf
Charles Merriam <charles.merriam@gmail.com>
2017-04-21 02:45:03 -0700

    Cleanup handy commands list and exterminate

--- src/deploy/exterminate.js
+++ src/deploy/exterminate.js
@@ -1,20 +1,15 @@
-import console from 'better-console';
 import Ocean from './ocean';
 import { line } from '../junkDrawer';
+import { d } from '../logging';
 
+async function exterminate() {
+  const ocean = new Ocean();
+  d(Array(5).join(line('=')));
+  const before = await ocean.prettyListDrops();
+  d(`Old droplets to drop:\n${before}`);
+  await ocean.destroyDrops();
+  const after = await ocean.prettyListDrops();
+  d(`Old droplets to drop:\n${after}`);
+}
 
-const ocean = new Ocean();
-console.log(Array(5).join(line('=')));
-
-ocean.listOfDrops()
-  .then((drops) => {
-    console.log('Dropping Old state:');
-    console.log(Ocean.prettyDrops(drops));
-    return ocean.destroyDrops();
-  })
-  .then(() => ocean.listOfDrops())
-  .then((drops) => {
-    console.log('New state:');
-    console.log(Ocean.prettyDrops(drops));
-  })
-  .catch(err => console.log('error: ', err));
+exterminate();
--- src/deploy/list.js
+++ src/deploy/list.js
@@ -1,12 +1,8 @@
 import console from 'better-console';
 import Ocean from './ocean';
-import line from '../junkDrawer';
+import { line } from '../junkDrawer';
 
 
 const ocean = new Ocean();
 console.log(Array(5).join(line('=')));
-
-ocean.listOfDrops()
-  .then((drops) => {
-    console.log(Ocean.prettyDrops(drops));
-  }).catch(err => console.log('error: ', err));
+ocean.prettyListDrops().then(drops => console.log(drops));
