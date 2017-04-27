83e8ecbae01ffb56e4847961f364b2f2d8b6f4ce
Charles Merriam <charles.merriam@gmail.com>
2017-04-26 18:48:54 -0700

    Clean up exterminate output

--- src/deploy/exterminate.js
+++ src/deploy/exterminate.js
@@ -4,12 +4,12 @@ import { d } from '../logging';
 
 async function exterminate() {
   const ocean = new Ocean();
-  d(Array(5).join(line('=')));
+  d(`\n${Array(5).join(line('='))}`);
   const before = await ocean.prettyListDrops();
   d(`Old droplets to drop:\n${before}`);
   await ocean.destroyDrops();
   const after = await ocean.prettyListDrops();
-  d(`Old droplets to drop:\n${after}`);
+  d(`Remaining:\n${after}`);
 }
 
 exterminate();
