34b17090315d3cbf7c15106830f0b58fc14b800a
Charles Merriam <charles.merriam@gmail.com>
2017-05-03 15:51:24 -0700

    Launch code mostly works
    Needs repeatable tests.

--- src/deploy/launch.js
+++ src/deploy/launch.js
@@ -1,13 +1,15 @@
-// because of node debugger problems
-// make 'tests' run then transfer.
-import assert from 'assert';
+// Launch a new running server.
+// This tends be sequential, and a bit monolithic, as fits the problem.
+// The output is chatty, as befits the use case.
+
+import assert from 'assert';  // sanity checks, even in running code
 import chalk from 'chalk';
+import rp from 'request-promise-native';
 import NodeSSH from 'node-ssh';
-import http from 'http';
 import Ocean from './ocean';
 import { d, ddir } from '../logging';
-// import { wait } from '../junkDrawer';
 import config from '../../private_config';
+import { wait } from '../junkDrawer';
 
 const ocean = new Ocean();
 const tag = 'stage';
@@ -16,14 +18,13 @@ const name = 'toy';
 
 function getIPFromResults(listDropsResults) {
   // get IP of the staging drop, or throw.
-  // const res = await ocean.listDrops(tag, name);
   const ip = listDropsResults[0].networks.v4[0].ip_address;
   return ip;
 }
 
 async function createStagingDrop() {
   await ocean.destroyDrops(tag);
-  const drop = await ocean.createDrop(tag, name);
+  await ocean.createDrop(tag, name);
   return true;
 }
 
@@ -45,6 +46,7 @@ async function getStagingIP() {
 
 async function getStagingSSH(ip, userName) {
   // return SSH handle for a new or existing server
+  // So, should this actually make a connection to see that it worked?
   const ssh = new NodeSSH();
   await ssh.connect({ host: ip,
     username: userName,
@@ -67,9 +69,10 @@ async function executeCommands(ssh, cmds) {
   }
 }
 
-async function foo() {
-  d('Checkpoint 1 - starting foo');
 
+function getScripts(userName) {
+  // return scripts { asRoot, asUser } each as arrays of lines of command.
+  // takes userName setting because it used in making the ssh key as well.
   const nodeVersion = '7.x';
   const installPackages = `
   # Add yarn repo
@@ -87,7 +90,6 @@ async function foo() {
   iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 8080
   `;
 
-  const userName = 'ps1';
   const userDir = `/home/${userName}`;
   const createUser = `
   adduser --disabled-password --gecos "" ${userName}
@@ -115,38 +117,55 @@ async function foo() {
   asUser = asUser.join('\n').split('\n').filter(x => x.trim());
   // make an array of individual, nonblank lines
 
+  return { asRoot, asUser };
+}
+
+function doesPageHaveTitle(html) {
+  return !!html.match(/The Bash Prompt/gm);
+}
+
+async function checkMainPageTitle(ip) {
+  // check that port 80 provides something.
+  const html = await rp.get(`http://${ip}`);
+  d('html', html);
+  return doesPageHaveTitle(html);
+}
+
+async function waitForServiceToStart(ip) {
+  for (let i = 0; i < 10; i += 1) {
+    if (await checkMainPageTitle(ip)) {
+      return true;
+    }
+    await wait(1000);
+  }
+  throw new Error('Web service did not start');
+}
+
+async function launchService() {
+  // launch the service on a new or existing staging drop.  returns ip.
+  const userName = 'ps1';
+
+  d('Checkpoint 1 - starting foo');
+
   const ip = await getStagingIP();
 
   d(`Current drops are:\n${await ocean.prettyListDrops()}`);
 
   d(`Checkpoint 2 - ip ${ip} obtained`);
+  const scripts = getScripts(userName);
 
   const rootSSH = await getStagingSSH(ip, 'root');
-
-  await executeCommands(rootSSH, asRoot);
+  await executeCommands(rootSSH, scripts.asRoot);
   rootSSH.dispose();
   d('Checkpoint 3 - root commands done');
 
   const ps1SSH = await getStagingSSH(ip, userName);
-  await executeCommands(ps1SSH, asUser);
+  await executeCommands(ps1SSH, scripts.asUser);
   ps1SSH.dispose();
   d('checkpoint 4 - user commands run');
-  const address = `http://${ip}:80`;
-  d(address);
 
-  // check that port 80 provides something.
-  http.get(`http://${ip}:80`, (res) => {
-    d(`status is ${res.statusCode} - ${res.statusName}`);
-    let body = '';
-    res.on('data', (data) => { body += data; });
-    res.on('end', () => {
-      console.log('body = ', body);
-      if (body.match(/The Bash Prompt/gm)) {
-        console.log('TEST PASSED');
-      }
-    });
-  });
-  d('99.   This program is gratified to be of use.');
+  await waitForServiceToStart();
+  return ip;
 }
 
 // ----- Tests ----------
@@ -164,6 +183,42 @@ function itShouldThrowOnBadResult() {
   assert.throws(() => getIPFromResults(listDropsNothingFoundResult));
 }
 
+function itShouldFindTheTitle() {
+  const goodHTML = `<!doctype html public "storage">
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta charset=utf-8/>
+<title>PS1 - The Bash Prompt</title>
+<link rel="stylesheet" href="/index.css" /> <!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+</head>
+<div id=app></div>
+<script src="/bundle.js"></script>`;
+  assert.equal(true, doesPageHaveTitle(goodHTML));
+}
+
+function itShouldFailTitleFindOnEmpty() {
+  const badHTML = '';
+  assert.equal(false, doesPageHaveTitle(badHTML));
+}
+
+itShouldFindTheTitle();
+itShouldFailTitleFindOnEmpty();
+
 itShouldGetTheIPAddress();
 itShouldThrowOnBadResult();
-foo();
+
+
+launchService()
+.then(ip => wait(2000, ip))
+.then(ip => checkMainPageTitle(ip))
+.then(() => console.log('Test Passed!'))
+.catch(err => console.log('Not so much...', err));
+
+d('99.   This program is gratified to be of use.');
